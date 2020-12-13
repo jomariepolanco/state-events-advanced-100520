ee8f20154b67db8b371004029efea0902089f0d3
jomariepolanco <jomariepolanco@gmail.com>
2020-12-13 17:24:48 -0500

    done

--- src/App.js
+++ src/App.js
@@ -10,35 +10,32 @@ class App extends React.Component {
     api: beyArray
   }
 
-  toggleCardFavorites = (id) => {
+  addFavorites = (id) => {
     // console.log(beyObj)
     let newArr = [...this.state.api]
     let favObj = newArr.find(bey => bey.id === id)
-
-    if (favObj.favorite === false) {
-      favObj.favorite = true 
-      this.setState((prevState) => ({api: newArr}))
-    } else if (favObj.favorite === true) {
-      favObj.favorite = false
-      this.setState({api: newArr})
-      window.alert('I got hot sauce in my bag, swag')
-    }
+    favObj.favorite = true 
+    this.setState({api: newArr})
   }
 
+  removeFavorites = (id) => {
+    let newArr = [...this.state.api]
+    let favObj = newArr.find(bey => bey.id === id)
+    favObj.favorite = false
+    this.setState({api: newArr})
+    window.alert('I got hot sauce in my bag, swag')
+  }
   favorites = () => {
     return [...this.state.api].filter(obj => obj.favorite)
   }
 
-  // notFavorites = () => {
-  //   return [...this.state.api].filter(obj => obj.favorite === false)
-  // }
 
   render() {
     // console.log(this.notFavorites())
     return (
       <div className="container" >
-        <BeyContainer cards={this.state.api} toggleCardFavorites={this.toggleCardFavorites} />
-        <Favorites cards={this.favorites()} toggleCardFavorites={this.toggleCardFavorites}/>
+        <BeyContainer cards={this.state.api} toggleCardFavorites={this.addFavorites} />
+        <Favorites cards={this.favorites()} toggleCardFavorites={this.removeFavorites}/>
       </div>
     );
   }
